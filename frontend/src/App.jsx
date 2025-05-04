94dad63a8335d2447b5897f8b9ab7e8ab5b8876b
Tapasshrestha2007 <tapasshrestha2007@gmail.com>
2025-05-04 22:43:57 +0545

    chore: add Tailwind CSS and Vite dependencies to package.json

---
+++ frontend/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Collection from './pages/collection'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Orders from './pages/Orders'
+import Product from './pages/Product'
+import PlaceOrder from './pages/PlaceOrder'
+
+
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Routes>
+        <Route path="/" element={<Home/>} /> 
+        <Route path="/about" element={<About/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/collection" element={<Collection/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/orders" element={<Orders/>} />
+        <Route path="/product/:productId" element={<Product/>} />
+        <Route path="/place-order" element={<PlaceOrder/>} />
+
+      </Routes>
+
+
+    </div>
+  )
+}
+
+export default App
